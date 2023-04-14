67f3b776035071d51195f19f96c47231e61b6979
Tia Doherty <doherty.tia@gmail.com>
2023-04-14 14:22:06 -0600

    fixed root directory

---
+++ backend/db/seeders/20230413194748-demo-user.js
@@ -0,0 +1,45 @@
+'use strict';
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users';
+    return queryInterface.bulkInsert(options, [
+      {
+        email: 'demo@user.io',
+        firstName: 'Testfirst',
+        lastName: 'Testlast',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user1@user.io',
+        firstName: 'Testonefirst',
+        lastName: 'Testonelast',
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'user2@user.io',
+        firstName: 'Testtwofirst',
+        lastName: 'Testtwolast',
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password3')
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+  }
+};
