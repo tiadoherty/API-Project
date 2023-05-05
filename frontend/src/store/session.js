6e76f31ba6284af82c7986ea02d52c813873505b
Tia Doherty <doherty.tia@gmail.com>
2023-05-05 11:52:59 -0600

    finished signup action thunk and testing

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -59,4 +59,22 @@ const sessionReducer = (state = initialState, action) => {
   }
 };
 
+//signup thunk
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 export default sessionReducer;
