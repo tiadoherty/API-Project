6ff7d3478f9c882b96e000f133f32a82d4195dc4
Tia Doherty <doherty.tia@gmail.com>
2023-05-05 11:45:31 -0600

    finished restore session user thunk

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -33,6 +33,14 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+//thunk to restore a user
+export const restoreUser = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session");
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
