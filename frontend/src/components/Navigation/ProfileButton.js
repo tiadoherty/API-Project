179b82348c4cbf2f6aa28c940f7b90379d29a715
Tia Doherty <doherty.tia@gmail.com>
2023-05-05 14:29:10 -0600

    finished profile button and drop down menu

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,32 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef();
 
-    const logout = (e) => {
-        e.preventDefault();
-        dispatch(sessionActions.logout());
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (!ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
     };
 
-    const ulClassName = "profile-dropdown";
+    document.addEventListener('click', closeMenu);
 
-    return (
-        <>
-            <button>
-                <i className="fas fa-user-circle" />
-            </button>
-            <ul className="profile-dropdown">
-                <li>{user.username}</li>
-                <li>{user.firstName} {user.lastName}</li>
-                <li>{user.email}</li>
-                <li>
-                    <button onClick={logout}>Log Out</button>
-                </li>
-            </ul>
-        </>
-    );
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+  return (
+    <>
+      <button onClick={openMenu}>
+        <i className="fas fa-user-circle" />
+      </button>
+      <ul className={ulClassName} ref={ulRef}>
+        <li>{user.username}</li>
+        <li>{user.firstName} {user.lastName}</li>
+        <li>{user.email}</li>
+        <li>
+          <button onClick={logout}>Log Out</button>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default ProfileButton;
