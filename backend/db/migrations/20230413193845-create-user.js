d76329b69d3f3a5a8c8f92d97c55d667a1d84e80
Tia Doherty <doherty.tia@gmail.com>
2023-05-07 20:04:12 -0600

    added options object to each migration file

--- backend/db/migrations/20230413193845-create-user.js
+++ backend/db/migrations/20230413193845-create-user.js
@@ -49,6 +49,6 @@ module.exports = {
     }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('Users', options);
   }
 };
--- backend/db/migrations/20230417195202-create-spot-image.js
+++ backend/db/migrations/20230417195202-create-spot-image.js
@@ -43,6 +43,6 @@ module.exports = {
     }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('SpotImages');
+    await queryInterface.dropTable('SpotImages', options);
   }
 };
