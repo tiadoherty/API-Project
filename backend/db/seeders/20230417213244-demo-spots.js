c2dd00e65c0eec7e49b50e03c6e2eda1b8637de3
Tia Doherty <doherty.tia@gmail.com>
2023-05-07 21:35:26 -0600

    Update seed files

--- backend/db/seeders/20230417213244-demo-spots.js
+++ backend/db/seeders/20230417213244-demo-spots.js
@@ -12,39 +12,51 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         ownerId: 1,
-        address: '123 Wine Street',
-        city: 'Sonoma',
-        state: 'California',
+        address: '34 Talokan Ct.',
+        city: 'Atlantis',
+        state: 'Hawaii',
         country: 'United States of America',
-        lat: 38.2919,
-        lng: 122.4580,
-        name: 'Napa Getaway',
-        description: 'Cute cottage to stay in while enjoying Sonoma vineyards',
-        price: 160
+        lat: 18.71,
+        lng: -158.27,
+        name: 'Underwater Temple',
+        description: 'Ancient underwater temple believed to be the center of the ruins of Atlantis',
+        price: 400
       },
       {
-        ownerId: 1,
-        address: '456 Otter Blvd',
-        city: 'Monterey',
-        state: 'California',
+        ownerId: 2,
+        address: '19 Red Dust Rd.',
+        city: 'Unit 8',
+        state: 'Mars Territory 4',
         country: 'United States of America',
-        lat: 36.6002,
-        lng: 121.8947,
-        name: 'Aquarium Apartment',
-        description: 'Enjoy walking to the Monterey Bay Aquarium!',
-        price: 135
+        lat: 0,
+        lng: 0,
+        name: 'Mars Habitation',
+        description: 'All the amenities of Earth, but with none of the earth.',
+        price: 84
+      },
+      {
+        ownerId: 2,
+        address: '100 Hill St.',
+        city: 'Unit 22',
+        state: 'Mars Territory 8',
+        country: 'United States of America',
+        lat: 0,
+        lng: 0,
+        name: 'Futuristic Pod Home',
+        description: 'Vertical pod habitat - Great for long term stays.',
+        price: 265
       },
       {
         ownerId: 3,
         address: '789 Beach Road',
-        city: 'Kailua',
-        state: 'Hawaii',
-        country: 'United States of America',
-        lat: 21.3946,
-        lng: 157.7434,
-        name: 'Beach Vacation Rental',
-        description: 'Beach fun for the whole family',
-        price: 350
+        city: 'Bora Bora',
+        state: 'Bora Bora',
+        country: 'French Polynesia',
+        lat: -16.5271,
+        lng: -151.7404,
+        name: 'H2Ome',
+        description: 'Beach fun for the whole family. NOTE: this property is underwater.',
+        price: 650
       }
     ], {})
 
@@ -60,7 +72,7 @@ module.exports = {
     options.tableName = 'Spots';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      name: { [Op.in]: ['Napa Getaway', 'Aquarium Apartment', 'Beach Vacation Rental'] }
+      ownerId: { [Op.in]: [1, 2, 3] }
     }, {})
   }
 };
