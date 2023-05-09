8770c88fc2e9b2d4d46f0fd7c7ee1381cbcc1740
Tia Doherty <doherty.tia@gmail.com>
2023-05-08 20:33:47 -0600

    Start create spot

---
+++ frontend/src/components/CreateSpotForm/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CreateSpotForm = () => {
+    return <h1>Create Spot Form</h1>
+}
+
+export default CreateSpotForm;
--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -48,12 +48,12 @@ function ProfileButton({ user }) {
             <ul className={ulClassName} ref={ulRef}>
                 {user ? (
                     <>
-                        <li>{user.username}</li>
-                        <li>{user.firstName} {user.lastName}</li>
+                        <li>Hello, {user.firstName}</li>
                         <li>{user.email}</li>
-                        <li>
-                            <button onClick={logout}>Log Out</button>
-                        </li>
+                        <li className="manage-spots">Manage Spots</li>
+                        <div className="logout-button-container">
+                            <button className="logout-button" onClick={logout}>Log Out</button>
+                        </div>
                     </>
                 ) : (
                     <>
